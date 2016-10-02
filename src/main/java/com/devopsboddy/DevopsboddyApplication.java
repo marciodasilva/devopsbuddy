d4bb300e75ebb310af36fa5fde8cc397243730a0
marciodasilva <marciodasilva@hotmail.com>
2016-10-02 10:27:35 -0700

    Fixed .gitignore file

---
+++ src/main/java/com/devopsboddy/DevopsboddyApplication.java
@@ -0,0 +1,12 @@
+package com.devopsboddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsboddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsboddyApplication.class, args);
+	}
+}
